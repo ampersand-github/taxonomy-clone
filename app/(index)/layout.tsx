41ba4cb4b9242e41c00d60b4275a8538fdf42758
ampersand-github <ampersand.dev@gmail.com>
2023-05-08 15:47:28 +0900

    ログインしているかどうかで出すアイコンを切り分ける

--- app/(index)/layout.tsx
+++ app/(index)/layout.tsx
@@ -4,7 +4,9 @@ import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
 import { MainNav } from "@/components/main-nav"
 import { SiteFooter } from "@/components/site-footer"
-import React from "react";
+import React from "react"
+import { getCurrentUser } from "@/lib/session"
+import { UserAccountNav } from "@/components/user-account-nav"
 
 interface MarketingLayoutProps {
   children: React.ReactNode
@@ -13,15 +15,33 @@ interface MarketingLayoutProps {
 export default async function MarketingLayout({
   children,
 }: MarketingLayoutProps) {
+  const user = await getCurrentUser()
+  console.log("user", user)
   return (
     <div className="flex min-h-screen flex-col">
       <header className="container z-40 bg-background">
         <div className="flex h-20 items-center justify-between py-6">
           <MainNav items={indexConfig.mainNav} />
           <nav>
-            <Link href="/login" className={cn(buttonVariants({ variant: "secondary", size: "sm" }), "px-4")}>
-              Login
-            </Link>
+            {user ? (
+              <UserAccountNav
+                user={{
+                  name: user.name,
+                  image: user.image,
+                  email: user.email,
+                }}
+              />
+            ) : (
+              <Link
+                href="/login"
+                className={cn(
+                  buttonVariants({ variant: "secondary", size: "sm" }),
+                  "px-4"
+                )}
+              >
+                Login
+              </Link>
+            )}
           </nav>
         </div>
       </header>
